44c87acd3d548a9c1ad442598ed1916334bf0cc2
Tyler Gosla <tyler.gosla@gmail.coM>
2018-10-27 16:28:35 -0400

    html to JSX

--- win-son-app/src/Footer/index.jsx
+++ win-son-app/src/Footer/index.jsx
@@ -1,13 +1,20 @@
-<footer>
-  <p newClass="subscribeToUs">Subscribe to Our Mailing List</p>
-  <button newClass="subscribe">Subscribe</button>
-  <a href="https://www.yelp.com/biz/win-son-brooklyn-4">
-    <img newClass="yelp" src="#" />
-  </a>
-  <a href="https://www.instagram.com/explore/locations/1020770799/win-son/?hl=en">
-    <img newClass="instagram" src="#" />
-  </a>
-  <a href="https://www.facebook.com/pages/Win-Son/196863394276454">
-    <img newClass="facebook" src="#" />
-  </a>
-</footer>
+let Foot = React.createClass({
+  render: function() {
+    return (
+
+      <footer>
+        <p newclass="subscribeToUs">Subscribe to Our Mailing List</p>
+        <button newclass="subscribe">Subscribe</button>
+        <a href="https://www.yelp.com/biz/win-son-brooklyn-4">
+          <img newclass="yelp" src="#" />
+        </a>
+        <a href="https://www.instagram.com/explore/locations/1020770799/win-son/?hl=en">
+          <img newclass="instagram" src="#" />
+        </a>
+        <a href="https://www.facebook.com/pages/Win-Son/196863394276454">
+          <img newclass="facebook" src="#" />
+        </a>
+      </footer>
+    );
+  }
+});
