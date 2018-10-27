07225e6990b82ae54175df5e91bbe242edf53e1e
Tyler Gosla <tyler.gosla@gmail.coM>
2018-10-26 23:09:43 -0400

    html and css of footer and reservations

---
+++ win-son-app/src/Footer/index.jsx
@@ -0,0 +1,13 @@
+<footer>
+  <p newClass="subscribeToUs">Subscribe to Our Mailing List</p>
+  <button newClass="subscribe">Subscribe</button>
+  <a href="https://www.yelp.com/biz/win-son-brooklyn-4">
+    <img newClass="yelp" src="#" />
+  </a>
+  <a href="https://www.instagram.com/explore/locations/1020770799/win-son/?hl=en">
+    <img newClass="instagram" src="#" />
+  </a>
+  <a href="https://www.facebook.com/pages/Win-Son/196863394276454">
+    <img newClass="facebook" src="#" />
+  </a>
+</footer>
---
+++ win-son-app/src/Reservations/index.jsx
@@ -0,0 +1,11 @@
+<link href='https://fonts.googleapis.com/css?family=Cabin Sketch' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Yanone Kaffeesatz' rel='stylesheet'>
+
+<h1 className="h1Font">Reservations</h2>
+
+<p className="pFont">For parties of more than 5, you can reserve any time!  Call us at 347 457 6010 or email us at reservations@winsonbrooklyn.com</p>
+</br>
+</br>
+<p className="pFont">For parties greater than 20 guests, we require a full-restaurant buyout.
+Please contact us for rates & packages.
+</p>
