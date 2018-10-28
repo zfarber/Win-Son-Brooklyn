8b0f267b785112debd5b30550eef2dcc57ae59e3
Misha Holzman <mcholzman1@gmail.com>
2018-10-28 18:03:30 -0400

    finished headerPhoto component

--- win-son-app/src/About/index.jsx
+++ win-son-app/src/About/index.jsx
@@ -1,67 +1,75 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
 class About extends Component {
   render() {
     return (
       <div>
-        <img className="title" src="https://i.imgur.com/HNglClT.png"/>
-      <div id="slider">
-        <div
-          id="carouselExampleControls"
-          className="carousel slide"
-          data-ride="carousel"
-          data-interval="5000"
-          keyboard="true"
-          data-pause="hover"
-        >
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                className="d-block"
-                src="https://i.imgur.com/1nAfDSX.jpg"
-                alt="First slide"
-                style={{width: '1048px', height: '695px'}}
-              />
+        <img className="title" src="https://i.imgur.com/HNglClT.png" />
+        <div id="slider">
+          <div
+            id="carouselExampleControls"
+            className="carousel slide"
+            data-ride="carousel"
+            data-interval="3000"
+            keyboard="true"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  className="d-block"
+                  src="https://i.imgur.com/1nAfDSX.jpg"
+                  alt="First slide"
+                  style={{ width: "1048px", height: "695px" }}
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src="https://i.imgur.com/QetKdt9.jpg"
+                  alt="Second slide"
+                  style={{ width: "100%", maxHeight: "695px" }}
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  className="d-block w-100"
+                  src="https://i.imgur.com/0aXkuSm.jpg"
+                  alt="Third slide"
+                  style={{ width: "100%", maxHeight: "695px" }}
+                />
+              </div>
             </div>
-            <div className="carousel-item">
-              <img
-                className="d-block w-100"
-                src="https://i.imgur.com/QetKdt9.jpg"
-                alt="Second slide"
-                style={{width: '100%', maxHeight: '695px'}}
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                className="d-block w-100"
-                src="https://i.imgur.com/0aXkuSm.jpg"
-                alt="Third slide"
-                style={{width: '100%', maxHeight: '695px'}}
-              />
-            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="prev"
+            >
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="next"
+            >
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="sr-only">Next</span>
+            </a>
           </div>
-          <a
-            className="carousel-control-prev"
-            href="#carouselExampleControls"
-            role="button"
-            data-slide="prev"
-          >
-            <span className="carousel-control-prev-icon" aria-hidden="true" />
-            <span className="sr-only">Previous</span>
-          </a>
-          <a
-            className="carousel-control-next"
-            href="#carouselExampleControls"
-            role="button"
-            data-slide="next"
-          >
-            <span className="carousel-control-next-icon" aria-hidden="true" />
-            <span className="sr-only">Next</span>
-          </a>
         </div>
-      </div>
+        <p className="text">
+          Win son is a taiwanese-american restaurant located in east
+          williamsburg, brooklyn. we're currently open for dinner and weekend
+          brunch.
+        </p>
+        <div className="social-links">
+          <img src="https://i.imgur.com/LNNlPGR.png" alt="" />
+          <img src="https://i.imgur.com/Npk8eja.png" alt="" />
+        </div>
       </div>
     );
   }
